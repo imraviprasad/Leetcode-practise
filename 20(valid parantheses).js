23532bb5667472c294480c0cff2b2da49041a050
raviprasad-aeq <raviprasad@aequalisys.com>
2024-03-04 01:14:42 +0530

    valid parentheses sum completed.

---
+++ 20(valid parantheses).js
@@ -0,0 +1,30 @@
+const validParantheses = (s) => {
+  const stack = [];
+  const parens = "() {} []";
+  let i = 0;
+
+  while (i < s.length) {
+    stack.push(s[i]);
+    i++;
+
+    let open = stack[stack.length - 2];
+    let closed = stack[stack.length - 1];
+
+    let potParens = open + closed;
+
+    console.log(open, closed);
+
+    if (parens.includes(potParens)) {
+      stack.pop();
+      stack.pop();
+    }
+  }
+
+  return stack.length === 0;
+};
+
+// console.log(validParantheses("()"));
+// console.log(validParantheses("()[]{}"));
+// console.log(validParantheses("(]"));
+// console.log(validParantheses("([)]"));
+// console.log(validParantheses("{[]}"));
